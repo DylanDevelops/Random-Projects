6e92847bc53dd63a385e3e9ae012dec3914d6451
DylanDev <dylanm.ravel@gmail.com>
2022-07-29 09:34:10 -0600

    adding the files

---
+++ C# Projects/Login and signup system/Program.cs
@@ -0,0 +1,98 @@
+﻿// Brings back to beginning
+bool loopBack = true;
+
+// commands
+string help = "-help";
+string signUp = "-signup";
+string login = "-login";
+string logOut = "-logout";
+string close = "-close";
+
+// storage of values
+string email = "notEntered";
+string password = "notEntered";
+
+//bools
+bool isLoggedIn = false;
+
+while(loopBack)
+{
+    Console.WriteLine($"Please choose a command. Type [{help}] if you need help.");
+    string answer1 = Console.ReadLine();
+
+    if(answer1 == signUp)
+    {
+        Console.WriteLine("Please Enter an Email:");
+        email = Console.ReadLine();
+
+        Console.WriteLine("\nPlease Enter a Password:");
+        password = Console.ReadLine();
+
+        Console.WriteLine($"\nYour email is: `{email}` and your password is `{password}`.\nIs this okay? Please type `yes` if the information is correct.");
+        string yesOrNo = Console.ReadLine();
+
+        if(yesOrNo == "yes")
+        {
+            isLoggedIn = true;
+            Console.WriteLine($"You are successfully logged in. If you wish to log out, type {logOut}");
+
+            if(isLoggedIn)
+            {
+                string logOutMessage = Console.ReadLine();
+
+                if(logOutMessage == logOut)
+                {
+                    Console.WriteLine($"Logging out of {email}...");
+                    isLoggedIn = false;
+                }
+            }
+        }
+        else if(yesOrNo == "no")
+        {
+            loopBack = false;
+        }
+    }
+    else if(answer1 == login)
+    {
+        Console.WriteLine("\nPlease Enter Email:");
+        string possibleEmail = Console.ReadLine();
+        
+        Console.WriteLine("\nPlease Enter Password:");
+        string possiblePassword = Console.ReadLine();
+
+        if(possibleEmail == email && possiblePassword == password)
+        {
+            isLoggedIn = true;
+            Console.WriteLine($"You are successfully logged in. If you wish to log out, type {logOut}");
+
+            if(isLoggedIn)
+            {
+                string logOutMessage = Console.ReadLine();
+
+                if(logOutMessage == logOut)
+                {
+                    Console.WriteLine($"Logging out of {email}...");
+                    isLoggedIn = false;
+                }
+            }
+        } 
+        else if(possibleEmail != email || possiblePassword != password)
+        {
+            Console.WriteLine("No account found. You must make an account.");
+        }
+    }
+    else if(answer1 == help)
+    {
+        Console.WriteLine($"The current commands are {login} or {signUp}\n");
+    }
+    else if(answer1 == close)
+    {
+        loopBack = false;
+    }
+    else
+    {
+        Console.WriteLine("Not a valid command.\nPlease try again.\n");
+    }
+}
+
+loopBack = false;
---
+++ C# Projects/RandomStuff/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        
+        // Password Username Enter
+
+        Console.WriteLine("Enter Username:");
+        string username = Console.ReadLine();
+
+        Console.WriteLine("\nEnter Password:");
+        string password = Console.ReadLine();
+        
+        int passwordLength = password.Length;
+
+        if(passwordLength < 10)
+        {
+            Console.WriteLine("\nThat password is way to easy to crack. \nTry Again. \n");
+            
+        }
+        Console.WriteLine($"Username: {username} \nPassword: {password}");
+
+        */
+
+        /*
+
+        // Calculator
+        Console.WriteLine("Enter a whole number:");
+        int numberOne = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"\nEnter a number to be multiplied by {numberOne}.");
+        int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+        int answer = numberOne * numberTwo;
+
+        Console.WriteLine($"\nAnswer: {numberOne} X {numberTwo} = {answer}");
+
+        */
+
+        
+
+        // Guess Number
+
+        int theNumber = 8;
+        int minimum = 1;
+        int maximum = 10;
+
+        Console.WriteLine("Guess a number between 1 and 10:");
+        
+        int guess = Convert.ToInt32(Console.ReadLine());
+        
+        while(guess <= theNumber - 1 || guess >= theNumber + 1)
+        {
+            Console.WriteLine("\nGuess Again:");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if(guess < minimum)
+            {
+                Console.WriteLine($"\n{guess} is below 1! Guess again:");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            if(guess > maximum)
+            {
+                Console.WriteLine($"\n{guess} is above 10! Guess again:");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine($"\nYou got it! The number was {theNumber}! You had to have been cheating. This was bulletproof!");
+    }
+}
