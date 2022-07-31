16795c1d7b346eb0d5821a97542c6ffd3d9ffbae
DylanDev <dylanm.ravel@gmail.com>
2022-07-30 22:43:13 -0600

    made a really small russian roulette game

---
+++ C# Projects/Console Apps/Russian Roulette/Program.cs
@@ -0,0 +1,17 @@
+﻿int playerNumber;
+int computerNumber;
+bool alive = true;;
+
+while(alive)
+{
+    Console.WriteLine("Pick a number between 1 and 6.");
+    playerNumber = Convert.ToInt32(Console.ReadLine());
+    computerNumber = Random.Shared.Next(1, 7);
+    Console.WriteLine($"The computer chose {computerNumber}");
+
+    if(playerNumber == computerNumber)
+    {
+        alive = false;
+        Console.WriteLine("\nBANG!\nYou are dead.\n");
+    }
+}
