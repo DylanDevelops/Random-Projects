90652d98a36c1539eba7b2fe4ab3f1981aa4f4b4
Dylan Ravel <dylanm.ravel@gmail.com>
2022-09-27 10:31:37 -0600

    push to cloud

--- C# Projects/Console Apps/Testing github copilot/Program.cs
+++ C# Projects/Console Apps/Testing github copilot/Program.cs
@@ -1,38 +1,8 @@
-﻿// create a system that censors curse words with asterisks (*)
-// the system should be able to censor any curse word in a sentence
+﻿int number;
 
-using System;
 
-namespace Censorship
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter a sentence to censor: ");
-            string sentence = Console.ReadLine();
-            Console.WriteLine("Enter a word to censor: ");
-            string word = Console.ReadLine();
-            string censored = Censor(sentence, word);
-            Console.WriteLine(censored);
-        }
+Console.WriteLine("Enter a number:");
 
-        static string Censor(string sentence, string word)
-        {
-            string[] words = sentence.Split(" ");
-            string censored = "";
-            foreach (string w in words)
-            {
-                if (w == word)
-                {
-                    censored += new string('*', word.Length) + " ";
-                }
-                else
-                {
-                    censored += w + " ";
-                }
-            }
-            return censored;
-        }
-    }
-}
+number = Console.ReadLine();
+
+Console.WriteLine(number + 10);
