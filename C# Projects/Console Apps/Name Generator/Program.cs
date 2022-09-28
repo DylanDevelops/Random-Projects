63c99e9d4cdee00c95c70bb17ba340bc492b7432
Dylan Ravel <dylanm.ravel@gmail.com>
2022-09-28 17:26:25 -0600

    upload

---
+++ C# Projects/Console Apps/Name Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿string enteredName;
+
+Console.WriteLine("Write your name!");
+enteredName = Console.ReadLine();
+
+if(enteredName == "Finn")
+{
+    Console.WriteLine("Damn you " + enteredName + "!");
+}
+else 
+{
+    Console.WriteLine("Hello " + enteredName + "!");
+}
+
